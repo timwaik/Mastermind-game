c6823be3a86ccd3e704dd9cdb16441e8d4e3d717
timwaik <timwaik@users.noreply.github.com>
2021-11-01 18:50:34 +0800

    Added more documentation

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # Mastermind-game
 
-Simple text based game made with Python, based on the game Mastermind. Players will have to guess a random n-digit code, with the numbers in the right order. Every right number in the correct place will score you one "Green" while a correct number in the wrong place will score you a "Yellow".
+Simple text based game made with Python, based on the game Mastermind.
+
+## Rules
+
+Players will have to guess a random n-digit code, with the numbers in the right order.
+Every right number in the correct place will score you one "Green" while a correct number in the wrong place will score you a "Yellow".
+Players win when they guess all Greens correctly.
 
 ### Things to note
 - The game has 3 difficulties that the player can choose from
--- mastermind.py
+++ mastermind.py
@@ -1,11 +1,67 @@
+"""
+A module for a text-based game of Mastermind.
+
+Classes:
+    Mastermind
+
+Functions:
+    run_game():
+    
+"""
+
 import random
 import time
 import sys
 
 
 class Mastermind:
-    """docstring for Mastermind.
-    introduction:"""
+    """
+    A class to represent an instance of a Mastermind game.
+
+        Attributes:
+        --------------------
+        difficulty : str
+            Current difficulty of the game.
+        difficultyTypes: str
+            Levels of difficulty available.
+        code: list
+            Code to be guessed by player.
+        input_code : list
+            Code attempted by the player.
+        loop_position : list
+            Helper list to determine number of right positions.
+        loop_number : list
+            Helper list to determine number of right numbers.
+        right_number : int
+            Number of correct numbers guessed (But with incorrect position) by the player this turn.
+        right_position : int
+            Number of absolute correct guesses (Correct number and postiion) by the player this turn.
+        turns : int
+            Number of turns used to guess the correct nunmbers.
+
+        Methods
+        --------------------
+        introduction(self):
+            Prints welcome message and details rules of the game.
+
+        continuer(self, statement, yesResult, noResult):
+            Helper function. Accepts [Yes] or [No] input from the player and returns either True or False.
+
+        difficulty_level(self):
+            Sets the difficulty level of the game.
+
+        new_turn(self):
+            Resets all attributes required for a new turn.
+
+        initialise_code(self):
+            Sets the code to be guessed by the player.
+
+        game(self):
+            Lets the player guess the code and returns the results of the guess.
+
+        restart_game(self):
+            Determine if a new game needs to be started.
+    """
 
     def __init__(self):
         super(Mastermind, self).__init__()
@@ -25,21 +81,40 @@ class Mastermind:
         self.turns = 0
 
     def introduction(self):
+        """
+        Prints welcome message and details rules of the game.
+
+            Parameters:
+                None
+            Returns:
+                None"""
+
         print("Welcome to the game!\n")
         time.sleep(1)
 
         print(
             """
-    THE RULES:
-    A secret code will be generated. Your job is to guess the code.
-    Numbers have to be in the right order!
-    If a number is in the right position, it is considered GREEN.
-    If a number is correct but in the wrong place, it is considered YELLOW.
-    """
+            THE RULES:
+            A secret code will be generated. Your job is to guess the code.
+            Numbers have to be in the right order!
+            If a number is in the right position, it is considered GREEN.
+            If a number is correct but in the wrong place, it is considered YELLOW.
+            """
         )
         time.sleep(1)
 
     def continuer(self, statement, yesResult, noResult):
+        """
+        Helper function. Accepts [Yes] or [No] input from the player and returns either True or False.
+
+        Parameters:
+            statement : str
+                statement to be answered by player
+            yesResult : str
+                statement to be displayed if player answers [Yes]
+            noResult : str
+                statement to be displayes if player answers [No]"""
+
         while True:
 
             yesno = input(statement)
@@ -57,12 +132,24 @@ class Mastermind:
             else:
                 print("Invalid input, try again")
 
-    def difficultyLevel(self):
+    def difficulty_level(self):
+        """
+        Sets the difficulty level of the game.
+
+        Informs player of current level and other difficulty levels available.
+        Takes user input to change difficulty level.
+
+            Parameters:
+                None
+            Returns:
+                None"""
+
         difficultyDisplay = list(self.difficultyTypes.keys())
         difficultyDisplay.remove(self.difficulty)
         print(
-            f"The current difficulty level is {self.difficulty}\n\
-                There are two other difficulty levels, {difficultyDisplay[0]} and {difficultyDisplay[1]}"
+            f"""
+            The current difficulty level is {self.difficulty}\n\
+            There are two other difficulty levels, {difficultyDisplay[0]} and {difficultyDisplay[1]}"""
         )
         statement = (
             "\nWould you like to change the difficulty level? Enter [Yes] or [No]\n"
@@ -88,6 +175,16 @@ class Mastermind:
             self.input_code = self.difficultyTypes[inputDifficulty.title()]
 
     def new_turn(self):
+        """
+        Resets all attributes required for a new turn.
+
+        Increments turn counter by 1
+
+            Parameters:
+                None
+            Returns:
+                None"""
+
         self.right_number = 0
         self.right_position = 0
         self.loop_position = []
@@ -95,6 +192,16 @@ class Mastermind:
         self.turns += 1
 
     def initialise_code(self):
+        """
+        Sets the code to be guessed by the player.
+
+        Each value is a random int ranging from 0-9.
+
+            Parameters:
+                None
+            Returns:
+                None"""
+
         print("Setting code...")
         for x in range(len(self.code)):
             self.code[x] = int(random.randint(0, 9))
@@ -102,81 +209,164 @@ class Mastermind:
         print(self.code)  # For debugging purposed, delete hash to display code
 
     def game(self):
+        """
+        Lets the player guess the code and returns the results of the guess.
+
+            Methods:
+                attempt():
+                    Receives input guesses from the Player.
+                correct_position():
+                    Determines number of absolute correct guesses (Correct number and postiion) by the player this turn.
+                correct_number():
+                    Determines number of correct numbers guessed (But with incorrect position) by the player this turn.
+                attempt_results():
+                    Prints results of the turn. Determines if the player wins the game.
+            Parameters:
+                None
+            Returns:
+                Boolean"""
+
         self.new_turn()
-        while True:
-            try:
-                input_string = input("Input a number, or type [quit] to exit game: \n")
-                if input_string.lower() == "quit":
-                    sys.exit("Goodbye!")
-                self.input_code = list(map(int, list(input_string)))
-                if len(input_string) != len(self.code):
-                    print("You didn't enter the right amount of numbers, try again")
+
+        def attempt():
+            """
+            Receives input guesses from the Player.
+
+            Guess is stored in self.input_code.
+
+            Catches incorrect values (strings, etc) and lengths. self.turns increments for every attempt.
+
+            Parameters:
+                None
+            Returns:
+                None"""
+
+            while True:
+                try:
+                    input_string = input(
+                        "Input a number, or type [quit] to exit game: \n"
+                    )
+                    if input_string.lower() == "quit":
+                        sys.exit("Goodbye!")
+                    self.input_code = list(map(int, list(input_string)))
+                    if len(input_string) != len(self.code):
+                        print("You didn't enter the right amount of numbers, try again")
+                        self.turns += 1
+                    else:
+                        break
+                except ValueError:
+                    print("You didn't input a number! Try again")
                     self.turns += 1
-                else:
-                    break
-            except ValueError:
-                print("You didn't input a number! Try again")
-                self.turns += 1
 
-        # Method 1 finding right position (use either one)
-        for x in range(len(self.code)):
-            if self.input_code[x] == self.code[x]:
-                self.loop_position.append(x)
-                self.right_position += 1
+        def correct_position():
+            """
+            Determines number of absolute correct guesses (Correct number and postiion) by the player this turn.
 
-        # Method 2 finding right position
-        """for (inputnumber, codenumber) in list(zip(self.input_code, self.code)):
-            if inputnumber == codenumber:
-                self.right_position += 1"""
+            Value is stored in self.right_position.
 
-        # Method for finding right number
-        for x in range(len(self.code)):
-            if self.input_code[x] in self.code and x not in self.loop_position:
-                bla = [
-                    index
-                    for index, number in enumerate(self.code)
-                    if number == self.input_code[x]
-                ]
-                for indexNumber in bla:
-                    if indexNumber not in self.loop_position:
-                        if indexNumber not in self.loop_number:
-                            self.loop_number.append(indexNumber)
-                            self.right_number += 1
-                            break
+            Parameters:
+                None
+            Returns:
+                None"""
 
-        print(
-            f"Green: {self.right_position}\n\
-              Yellow: {self.right_number}"
-        )
+            # Method 1 finding right position (use either one)
+            for x in range(len(self.code)):
+                if self.input_code[x] == self.code[x]:
+                    self.loop_position.append(x)
+                    self.right_position += 1
+            # Method 2 finding right position
+            """for (inputnumber, codenumber) in list(zip(self.input_code, self.code)):
+                if inputnumber == codenumber:
+                    self.right_position += 1"""
 
-        if self.right_position == len(self.code):
+        def correct_number():
+            """
+            Determines number of numbers guessed (But with incorrect position) by the player this turn
+
+            Value is stored in self.right_number.
+
+            Parameters:
+                None
+            Returns:
+                None"""
+
+            # Method for finding right number
+            for x in range(len(self.code)):
+                if self.input_code[x] in self.code and x not in self.loop_position:
+                    indexes = [
+                        index
+                        for index, number in enumerate(self.code)
+                        if number == self.input_code[x]
+                    ]
+                    for indexNumber in indexes:
+                        if indexNumber not in self.loop_position:
+                            if indexNumber not in self.loop_number:
+                                self.loop_number.append(indexNumber)
+                                self.right_number += 1
+                                break
+
+        def attempt_results():
+            """
+            Prints results of the turn. Determines if the player wins the game.
+
+            Parameters:
+                None
+            Returns:
+                Boolean"""
+
             print(
-                f"You win, congratulations!\n\
-                    Number of tries taken: {self.turns}"
+                f"""
+                Green: {self.right_position}
+                Yellow: {self.right_number}"""
             )
-            return True
-        else:
-            return False
 
-    def end_game(self):
-        self.turns = 0
+            if self.right_position == len(self.code):
+                print(
+                    f"You win, congratulations!\n\
+                        Number of tries taken: {self.turns}"
+                )
+                self.turns = 0
+                return True
+            else:
+                return False
+
+        attempt()
+        correct_position()
+        correct_number()
+        return attempt_results()
+
+    def restart_game(self):
+        """
+        Determine if a new game needs to be started.
+
+        Parameters:
+                None
+            Returns:
+                Boolean"""
+
         statement = "Do you want to play again? Enter [yes] or [no]\n"
         yesResult = "Starting again!"
         noResult = "Okay, we'll miss you. Come back soon! :("
-        return self.continuer(statement, yesResult, noResult) == True
+        return self.continuer(statement, yesResult, noResult)
 
 
-def main():
+def run_game():
+    """
+    Starts an instance of a Mastermind game.
+
+    Parameters:
+        None
+
+    Returns:
+        None"""
+
     p1 = Mastermind()
     p1.introduction()
     while True:
-        p1.difficultyLevel()
+        p1.difficulty_level()
         p1.initialise_code()
         while True:
-            if p1.game() == True:
+            if p1.game():
                 break
-        if p1.end_game() == False:
+        if not p1.restart_game():
             break
-
-
-main()
